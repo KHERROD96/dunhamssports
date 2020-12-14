1d7806073918924482569c0ac27a68371903963d
KHERROD96 <koraherrod@gmail.com>
2020-12-13 22:39:06 -0600

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import homeImage from './images/homeImage.png';
  
 class Home extends Component {
   render() {
@@ -6,10 +7,13 @@ class Home extends Component {
       <div>
         <h2>Get in the game at Dunham's Sports, where our big names bring you in and our low prices bring you back!</h2>
     
-        <ul>
-            <li>Free Curbside Or In-Store Pick-Up</li>
-            <li>Hot Deals And Savings This Season</li>
-        </ul>
+        <p>Our motto at Dunham's Sports is <b>Big Names...Low Prices</b>. That is what we do everyday, give our customers the choice of the name brands that they want at the lowest prices possible. </p>
+        
+        <h2>What do we offer</h2>
+        
+        <p>Dunham's Sports offers a full line of traditional sporting goods and athletic equipment as well as a wide variety of active and casual sports apparel and footwear. Take advantage of our <b>FREE CURBSIDE</b> or <b>IN-STORE PICK-UP</b> and deals that you won't want to miss out on!  </p>
+        
+        <img src={homeImage} class="image1"/>
       </div>
     );
   }
--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import walkingboots from './images/walkingboots.png';
  
 class Shop extends Component {
   render() {
     return (
       <div>
-        <h2>Select Department</h2>
-        <ul>
+        <h2>Departments</h2>
+        <br />
           <li><a href="">Footwear</a></li>
+          <ul><img src={walkingboots} class="image2"/></ul>
+          <ul>Walking Boots</ul>
+          <ul>Price: $39.95</ul>
+          <ul><button>Add to Cart</button></ul>
           <li><a href="">Apparel</a></li>
           <li><a href="">Sports</a></li>
           <li><a href="">Men</a></li>
@@ -17,7 +22,7 @@ class Shop extends Component {
           <li><a href="">Accessories</a></li>
           <li><a href="">Deals</a></li>
           <li><a href="">Clearance</a></li>
-        </ul>
+        
       </div>
     );
   }
